458e8734f15a7e04265e5c957458afc03a1f7c90
Zaryus <96929601+ZaryusDev@users.noreply.github.com>
2022-01-07 23:04:54 +0100

    Correction d'erreur
    L'erreur était que le fichier était introuvable lors du build de l'application dès qu'il est en fichier racine, le problème est résolut !

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { RCPDiscord } = require('./src/RPCDiscord.js')
 var RCP = new RCPDiscord()
 
 
-const db = new sqlite3.Database('./src/database.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE)
+const db = new sqlite3.Database('./database.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE)
 const Eapp = express();
 
 //DB init
